ab6fe22eb3eb87599a358dd4414aeb7b32251ddf
borvux <bennyjoram@email.com>
2024-07-11 11:41:58 -0500

    tested import map with assest pipeline to toggle

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import { Turbo } from "@hotwired/turbo-rails"
 import "controllers"
 
@@ -9,3 +10,10 @@ window.jQuery = jquery;
 window.$ = jquery;
 import Rails from "@rails/ujs"
 Rails.start();
+
+import { toggleTextVisibility } from "./custom/toggle_visibility";
+
+document.addEventListener('DOMContentLoaded', () => {
+  // attach it to window
+  window.toggleTextVisibility = toggleTextVisibility;
+});
